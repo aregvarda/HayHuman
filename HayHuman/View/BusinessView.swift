93c438699052a98028ac66d5d6b2b907ff99ee80
Areg Vardanian <aregv@bk.ru>
2021-12-30 17:26:02 +0300

    fix glitching

--- HayHuman/View/BusinessView.swift
+++ HayHuman/View/BusinessView.swift
@@ -25,44 +25,39 @@ struct BusinessView: View {
     var body: some View {
         ScrollView {
             if businessDetail.showingBusinessProfile == false && businessDetail.selectedBusinessProfile == nil {
-                VStack {
+                VStack() {
                     HStack(spacing: 16) {
                         LazyVGrid(columns: gridLayout, spacing: 25) {
                             ForEach(cardBusiness) { cardbus in
                                 BusinessCardView(cardbus: cardbus)
                                     .onTapGesture {
-                                        withAnimation(.spring()) {
+                                        withAnimation(.easeOut(duration: 0.1)) {
+                                            feedback.impactOccurred()
+                                        }
                                             businessDetail.selectedBusinessProfile = cardbus
                                             businessDetail.showingBusinessProfile = true
-                                            businessDetail.showingBusinessProfile.toggle()
+                                           
                                             
-                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                                loadView.toggle()
-                                            }
+                                            
                                         }
                                     }
-                                    .matchedGeometryEffect(id: cardbus.id, in: namespace)
+                                    
                                     .frame(height: 180)
                                     .cornerRadius(15)
+                                   
                             }
                         }
                     }.padding()
-                }
+                    
             } else {
                 BusinessDetailList()
+                }
             }
         }
     }
-}
 
-struct CardBusiness: Encodable, Decodable, Hashable, Identifiable {
-    static func == (lhs: CardBusiness, rhs: CardBusiness) -> Bool {
-        return lhs.imageName == rhs.imageName && lhs.title == rhs.title
-    }
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(imageName)
-        hasher.combine(title)
-    }
+
+struct CardBusiness: Codable, Identifiable {
     
     var id: Int
     let title: String
@@ -74,46 +69,14 @@ struct CardBusiness: Encodable, Decodable, Hashable, Identifiable {
 }
 
 let cardBusiness: [CardBusiness] = Bundle.main.decode("business.json")
-let businessCards: CardBusiness = cardBusiness[1]
-//    CardBusiness(title: "gulbenkian",
-//                 imageName: "gulbenkian", name: "Calouste Gulbenkian", city: "Constantinople, Ottoman Empire", titleDetail: "A British-Armenian businessman and philanthropist.", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "libmann",
-//                 imageName: "libmann", name: "test1", city: "test1", titleDetail: "test1", description: "test1", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "manukyan",
-//                 imageName: "manukyan", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "ernekyan",
-//                 imageName: "ernekyan", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "kerkorian",
-//                 imageName: "kerkorian", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "vardanian",
-//                 imageName: "vardanian", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "afeyan",
-//                 imageName: "afeyan", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "hovnanian",
-//                 imageName: "hovnanian", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "mantashev",
-//                 imageName: "mantashev", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "сafesjian",
-//                 imageName: "сafesjian", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "sir paul",
-//                 imageName: "sir_paul", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "aramyants",
-//                 imageName: "aramyants", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "karapetyan",
-//                 imageName: "karapetyan", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "sirmakes",
-//                 imageName: "sirmakes", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "tchuruk",
-//                 imageName: "tchuruk", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922)),
-//    CardBusiness(title: "manoukian",
-//                 imageName: "manoukian", name: "test", city: "test", titleDetail: "test", description: "test", coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922))
-//]
+let businessCards: CardBusiness = cardBusiness[0]
+
 
 struct BusinessCardView: View {
     let cardbus: CardBusiness
     var body: some View {
         GeometryReader { proxy in
-            ZStack {
+            ZStack(alignment: .bottom) {
                 Image(cardbus.imageName)
                     .resizable()
                     .scaledToFill()
@@ -130,6 +93,7 @@ struct BusinessCardView: View {
                     .multilineTextAlignment(.center)
                     .padding(7)
                     .foregroundColor(.white)
+                    .shadow(color: .gray, radius: 4, x: 0, y: 0)
             }
         }
     }
