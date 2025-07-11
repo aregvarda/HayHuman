fc916fd802c1f3b6df9c7e63934023f283b528cc
Areg Vardanian <91129638+aregvarda@users.noreply.github.com>
2025-07-11 06:26:49 -0700

    Create MilitaryInDepth.swift

---
+++ HayHuman/HayHuman/Model/MilitaryDetail/MilitaryInDepth.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MilitaryInDepth: View {
+    var body: some View {
+        MilitaryDetailList()
+    }
+}
+
+struct MilitaryInDepth_Previews: PreviewProvider {
+    static var previews: some View {
+        MilitaryInDepth()
+    }
+}
