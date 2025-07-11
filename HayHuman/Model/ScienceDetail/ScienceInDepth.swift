a39646b265365561eba1c34088a88579a94e09fd
Areg Vardanian <91129638+aregvarda@users.noreply.github.com>
2025-07-11 06:24:33 -0700

    Create ScienceInDepth.swift

---
+++ HayHuman/Model/ScienceDetail/ScienceInDepth.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ScienceInDepth: View {
+    var body: some View {
+        ScienceDetailList()
+    }
+}
+
+struct ScienceInDepth_Previews: PreviewProvider {
+    static var previews: some View {
+        ScienceInDepth()
+    }
+}
